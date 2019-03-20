4a08118a53ce7d7d9aa27c70cc05700615155a74
Modernkennnern <kenneth.98@hotmail.com>
2019-03-20 18:03:58 +0100

    Removed support for Electric Vehicles: Reborn

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,6 @@
 if settings.startup["non-combat-mode"].value == false then
+	
+	--Updates equipment grids to support the various bob's vehicle grids, within reason.
 	if data.raw["equipment-category"]["armoured-vehicle"] then 
 		table.insert(data.raw["equipment-grid"]["CargoPlane-equipment-grid"].equipment_categories,"vehicle")
 		if settings.startup["aircraftCargoPlane-equipment-armoured"].value == true then
