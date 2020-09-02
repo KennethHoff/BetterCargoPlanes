4e5b14bc15c047ba462835afd6584dbd5b5e811c
Kenneth Hoff <kenneth.98@hotmail.com>
2020-09-02 20:13:36 +0200

    Added configuration options for inventory sizes of the Cargo Planes - including the default plane in Aircraft Updated Readme (slightly)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # BetterCargoPlanes
-Improves the Cargo Plane from the Aircraft mod to have an Equipment Slot. I always preferred the Cargo Plane, but it felt useless without an Equipment 
+Improves the Cargo Plane from the Aircraft mod to have an Equipment Slot. I always preferred the Cargo Plane, but it felt useless without being able to use Equipment
--- settings.lua
+++ settings.lua
@@ -5,5 +5,33 @@ data:extend({
         setting_type = "startup",
         default_value = false,
         order="aa",
-    }
+    },
+    
+    -- Inventory Size Configuration
+
+
+
+    {
+        type = "int-setting",
+        name = "betterCargoPlanes-inventorySizeT1",
+        setting_type = "startup",
+        default_value = 120,
+        order="ia"
+    },
+    {
+        type = "int-setting",
+        name = "betterCargoPlanes-inventorySizeT2",
+        setting_type = "startup",
+        default_value = 180,
+        order="ia"
+    },
+
+    {
+        type = "int-setting",
+        name = "betterCargoPlanes-inventorySizeT3",
+        setting_type = "startup",
+        default_value = 240,
+        order="ia"
+    },
+    
 })
