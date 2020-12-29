65f746f926cfb8c39b0b1919371f34fc5cf8add9
Kenneth Hoff <kenneth.98@hotmail.com>
2020-12-29 13:30:28 +0100

    Removed unused code

--- prototypes/compatability/Krastorio2_Equipment.lua
+++ prototypes/compatability/Krastorio2_Equipment.lua
@@ -3,10 +3,6 @@ local function AddEquipmentCategories(equipment_category)
 		table.insert(data.raw["equipment-grid"]["Cargo-Plane-Equipment-Grid"].equipment_categories, equipment_category)
 		table.insert(data.raw["equipment-grid"]["Better-Cargo-Plane-Equipment-Grid"].equipment_categories, equipment_category)
 		table.insert(data.raw["equipment-grid"]["Even-Better-Cargo-Plane-Equipment-Grid"].equipment_categories, equipment_category)
-
-		if data.raw["equipment-grid"]["kr-cargo-plane-grid"] then
-			table.insert(data.raw["equipment-grid"]["kr-cargo-plane-grid"].equipment_categories, equipment_category)
-		end
 	end
 end
 
